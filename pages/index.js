ae7bcd03a8ff0440632f78f2f637869feb872452
Aneesh Laiwala <laiwala@hotmail.com>
2025-04-02 13:34:14 +0530

    Working starter project

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🎉 SurveySphere is Live!</h1>
+      <p>Welcome to your immersive survey platform storyboard prototype.</p>
+    </div>
+  );
+}
